646776c053a3f93f65731bc28594e24b7e01b697
Honoka Guo <guoyuan.p@foxmail.com>
2024-11-15 15:40:27 +0800

    增加了ResponseBase,大家可以把自己的entity使用pushData方法一个一个推入。用于前后传递

---
+++ src/main/java/com/example/backend/entity/TodoRecord.java
@@ -0,0 +1,22 @@
+package com.example.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoRecord {
+    private int id;
+    private String title;
+    private String content;
+    private int user_id;
+
+    public TodoRecord(int id, String title, String content, int user_id) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user_id = user_id;
+    }
+
+}
