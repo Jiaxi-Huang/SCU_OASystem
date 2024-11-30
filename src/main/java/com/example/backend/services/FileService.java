d5ddf7b312b127429df952210bf5526663e1a598
陈春宇 <127867563+Bocchi0@users.noreply.github.com>
2024-11-30 20:25:17 +0800

    Add files via upload

---
+++ src/main/java/com/example/backend/services/FileService.java
@@ -0,0 +1,54 @@
+package com.example.backend.services;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.backend.entity.Files;
+import com.example.backend.mapper.FileMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FileService extends ServiceImpl<FileMapper, Files> {
+
+    @Autowired
+    private FileMapper fileMapper;
+    public List<Files> getFileByUserId(int userId) {
+        return  fileMapper.getFileByUserId(userId);
+    }
+
+
+    public int moveFile(Files record) {
+        int res_code = 0;
+        for (int id : record.getIds()) {
+            res_code = fileMapper.moveFile(id, record.getDirId());
+        }
+        return res_code;
+    }
+
+    public int delFolder(Files record) {
+        int res_code = 0;
+        for (int id : record.getIds()) {
+            res_code = fileMapper.delFolder(id);
+        }
+        return res_code;
+    }
+
+    public int remarkFile(Files record) {
+        System.out.println(record.getIds());
+        int res_code = 0;
+        for (int id : record.getIds()) {
+            res_code = fileMapper.remarkFile(id,record.getRemark());
+        }
+        return res_code;
+    }
+
+
+    public int uploadFile(Files record) {
+        System.out.println(record.getFileName());
+        int res_code = fileMapper.uploadFile(record.getFileName(),record.getExt(),
+                    record.getSize(),record.getDirId(),record.getUserId(),record.getUrl());
+
+        return res_code;
+    }
+}
