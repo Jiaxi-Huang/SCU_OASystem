35bff7ced0c8f6ced0da8bb374e89cfd41d18f89
陈春宇 <127867563+Bocchi0@users.noreply.github.com>
2024-11-30 20:32:42 +0800

    Create personnalFile.sql

---
+++ sql/personnalFile.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `files` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) NOT NULL,
+  `ext` varchar(10),
+  `size` int NOT NULL,
+  `dir_id` int,
+  `url` varchar(1024) NOT NULL,
+  `remark` text,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE `folders` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `pid` int DEFAULT NULL,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `user_id` int NOT NULL,
+    PRIMARY KEY (`id`)
+) 
