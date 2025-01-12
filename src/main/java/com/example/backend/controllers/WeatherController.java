e4a7a964a37feca4e26cf0913e520bd7fd0e091d
SLPC-SHUAI <2825834235@qq.com>
2025-01-12 13:08:54 +0800

    注释修改

--- src/main/java/com/example/backend/controllers/WeatherController.java
+++ src/main/java/com/example/backend/controllers/WeatherController.java
@@ -16,14 +16,11 @@ public class WeatherController {
 
     @GetMapping
     public Weather getWeather(@RequestParam String city, @RequestParam String date) {
-//        System.out.println("Received city parameter: " + city); // 输出接收到的城市参数
-//        System.out.println("Received date parameter: " + date); // 输出接收到的日期参数
         LocalDate formattedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         Weather weather = weatherService.getWeatherByCityAndDate(city, formattedDate);
         if (weather == null) {
             throw new RuntimeException("Weather data not found for city: " + city + " on date: " + formattedDate);
         }
-//        System.out.println("Weather data from database for city " + city + " on date " + formattedDate + ": " + weather);
         return weather;
     }
 }
